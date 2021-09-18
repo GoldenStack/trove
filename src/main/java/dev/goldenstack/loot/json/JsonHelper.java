79dc0c59aae6c7363b1b30709775411e92157c89
GoldenStack <goldenfire64yt@gmail.com>
2021-09-18 13:00:42 -0500

    Update JsonHelper with the ability to get the message that would be used

--- src/main/java/dev/goldenstack/loot/json/JsonHelper.java
+++ src/main/java/dev/goldenstack/loot/json/JsonHelper.java
@@ -16,6 +16,77 @@ public class JsonHelper {
     }
 
     /**
+     * @return The error message, for {@code JsonParseException}s, when an element should not have been null.
+     */
+    public static @NotNull String notNullMessage(@NotNull String key){
+        return "Expected value for key \""+key+"\" but found null!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonPrimitive but should
+     * have been.
+     */
+    public static @NotNull String notJsonPrimitiveMessage(@NotNull String key){
+        return "Expected primitive value (string, number, boolean) for key \""+key+"\" but found complex object!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonObject but should
+     * have been.
+     */
+    public static @NotNull String notJsonObjectMessage(@NotNull String key){
+        return "Expected object for key \""+key+"\" but found other value (primitive, array, null, etc)!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonPrimitive and a string
+     * but should have been.
+     */
+    public static @NotNull String notStringMessage(@NotNull String key){
+        return "Expected string for key \""+key+"\" but found other value (number, object, array, boolean, etc)!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonPrimitive and a boolean
+     * but should have been.
+     */
+    public static @NotNull String notBooleanMessage(@NotNull String key){
+        return "Expected boolean for key \""+key+"\" but found other value (number, object, array, string, etc)!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonPrimitive and a valid
+     * {@code NamespaceID} but should have been.
+     */
+    public static @NotNull String notNamespaceIdMessage(@NotNull String key){
+        return "Expected Namespace ID at key \""+key+"\" but received string with more than one colon!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonPrimitive and a valid
+     * {@code UUID} but should have been.
+     */
+    public static @NotNull String notUuidMessage(@NotNull String key){
+        return "Expected UUID for key \""+key+"\" but found other value (number, object, array, boolean, etc)!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonPrimitive and a valid
+     * {@code NamespaceID} but should have been.
+     */
+    public static @NotNull String invalidUuidMessage(@NotNull String key, @NotNull String value){
+        return "Expected UUID for key \""+key+"\" but found invalid value \""+value+"\"!";
+    }
+
+    /**
+     * @return The error message, for {@code JsonParseException}s, when an element was not a JsonArray but should have
+     * been.
+     */
+    public static @NotNull String notJsonArrayMessage(@NotNull String key){
+        return "Expected array for key \""+key+"\" but found other value (primitive, object, null, etc)!";
+    }
+
+    /**
      * Utility method to test if a JsonElement is null or is JsonNull<br>
      * @param element The element to test
      * @return True if the element is null or is an instance of JsonNull, otherwise false
@@ -33,7 +104,7 @@ public class JsonHelper {
      */
     public static @NotNull JsonElement assureNotNull(@Nullable JsonElement element, @NotNull String key) throws JsonParseException {
         if (isNull(element)){
-            throw new JsonParseException("Expected value for key \""+key+"\" but found null!");
+            throw new JsonParseException(notNullMessage(key));
         }
         return element;
     }
@@ -47,7 +118,7 @@ public class JsonHelper {
      */
     public static @NotNull JsonPrimitive assureJsonPrimitive(@NotNull JsonElement element, @NotNull String key) throws JsonParseException {
         if (!element.isJsonPrimitive()){
-            throw new JsonParseException("Expected primitive value (string, number, boolean) for key \""+key+"\" but found complex object!");
+            throw new JsonParseException(notJsonPrimitiveMessage(key));
         }
         return element.getAsJsonPrimitive();
     }
@@ -61,7 +132,7 @@ public class JsonHelper {
      */
     public static @NotNull JsonObject assureJsonObject(@NotNull JsonElement element, @NotNull String key) throws JsonParseException {
         if (!element.isJsonObject()){
-            throw new JsonParseException("Expected object for key \""+key+"\" but found other value (primitive, array, null, etc)!");
+            throw new JsonParseException(notJsonObjectMessage(key));
         }
         return element.getAsJsonObject();
     }
@@ -78,7 +149,7 @@ public class JsonHelper {
         if (primitive.isString()){
             return primitive.getAsString();
         } else {
-            throw new JsonParseException("Expected string for key \""+key+"\" but found other value (number, object, array, boolean, etc)!");
+            throw new JsonParseException(notStringMessage(key));
         }
     }
 
@@ -94,7 +165,7 @@ public class JsonHelper {
         if (primitive.isBoolean()){
             return primitive.getAsBoolean();
         } else {
-            throw new JsonParseException("Expected boolean for key \""+key+"\" but found other value (number, object, array, string, etc)!");
+            throw new JsonParseException(notBooleanMessage(key));
         }
     }
 
@@ -111,7 +182,7 @@ public class JsonHelper {
         if (index < 0){
             return NamespaceID.from(NamespaceID.MINECRAFT_NAMESPACE, id);
         } else if (id.indexOf(':', index + 1) != -1){
-            throw new JsonParseException("Expected Namespace ID at key \""+key+"\" but received string with more than one colon!");
+            throw new JsonParseException(notNamespaceIdMessage(key));
         } else {
             return NamespaceID.from(id.substring(0, index), id.substring(index + 1));
         }
@@ -130,10 +201,10 @@ public class JsonHelper {
             try {
                 return UUID.fromString(val);
             } catch(IllegalArgumentException exception){
-                throw new JsonParseException("Expected UUID for key \"id\" but found invalid value \""+val+"\"!");
+                throw new JsonParseException(invalidUuidMessage(key, val));
             }
         }
-        throw new JsonParseException("Expected UUID for key \""+key+"\" but found other value (number, object, array, boolean, etc)!");
+        throw new JsonParseException(notUuidMessage(key));
     }
 
     /**
@@ -145,7 +216,7 @@ public class JsonHelper {
      */
     private static @NotNull JsonArray assureJsonArray(@NotNull JsonElement element, @NotNull String key) throws JsonParseException {
         if (!element.isJsonArray()){
-            throw new JsonParseException("Expected array for key \""+key+"\" but found other value (primitive, object, null, etc)!");
+            throw new JsonParseException(notJsonArrayMessage(key));
         }
         return element.getAsJsonArray();
     }
