198c9a926b7747ddffcb0848f88f79757c003b4e
GoldenStack <goldenfire64yt@gmail.com>
2021-10-02 12:53:20 -0500

    LootFunction

---
+++ src/main/java/dev/goldenstack/loot/function/LootFunction.java
@@ -0,0 +1,21 @@
+package dev.goldenstack.loot.function;
+
+import dev.goldenstack.loot.context.LootContext;
+import dev.goldenstack.loot.json.LootSerializer;
+import net.minestom.server.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.BiFunction;
+
+/**
+ * Represents something that can make modifications to an ItemStack based on the provided LootContext.
+ */
+public interface LootFunction extends LootSerializer<LootFunction>, BiFunction<ItemStack, LootContext, ItemStack> {
+
+    /**
+     * Makes modifications to the provided ItemStack if needed and returns the result.<br>
+     * The only reason this is being overridden is so that people have to use {@link NotNull @NotNull}
+     */
+    @Override
+    @NotNull ItemStack apply(@NotNull ItemStack itemStack, @NotNull LootContext context);
+}
