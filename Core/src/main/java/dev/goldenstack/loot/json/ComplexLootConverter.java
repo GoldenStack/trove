1d8515cae1b65e5aae2e1fd97081ef5129d675da
GoldenStack <goldenfire64yt@gmail.com>
2022-07-03 02:13:21 -0500

    Implement complex loot converters
    Signed-off-by: GoldenStack <goldenfire64yt@gmail.com>

---
+++ Core/src/main/java/dev/goldenstack/loot/json/ComplexLootConverter.java
@@ -0,0 +1,40 @@
+package dev.goldenstack.loot.json;
+
+import com.google.gson.JsonElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Handles complex conversion where nothing is guaranteed about provided objects.
+ * @param <L> the loot item
+ * @param <T> the class of the object that will be provided
+ */
+public abstract class ComplexLootConverter<L, T extends LootAware<L>> {
+
+    /**
+     * @return true if this converter should be used to deserialize the provided element
+     */
+    public abstract boolean canDeserialize(@Nullable JsonElement element, @NotNull LootConversionManager<L, T> converter);
+
+    /**
+     * @param element the element to deserialize
+     * @param converter the exact converter that is requesting deserialization
+     * @return the instance of {@link T} that was created
+     * @throws LootParsingException if, for some reason, something goes wrong while deserializing
+     */
+    public abstract @NotNull T deserialize(@Nullable JsonElement element, @NotNull LootConversionManager<L, T> converter) throws LootParsingException;
+
+    /**
+     * @return true if this converter should be used to serialize the provided element
+     */
+    public abstract boolean canSerialize(@NotNull T input, @NotNull LootConversionManager<L, T> converter);
+
+    /**
+     * @param input the input that needs to be serialized
+     * @param converter the exact converter that is requesting serialization
+     * @return the JSON element that was created
+     * @throws LootParsingException if, for some reason, something goes wrong while serializing
+     */
+    public abstract @NotNull JsonElement serialize(@NotNull T input, @NotNull LootConversionManager<L, T> converter) throws LootParsingException;
+
+}
--- Core/src/main/java/dev/goldenstack/loot/json/LootConversionManager.java
+++ Core/src/main/java/dev/goldenstack/loot/json/LootConversionManager.java
@@ -9,11 +9,7 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.BiFunction;
+import java.util.*;
 
 /**
  * Manages serialization and deserialization ("conversion") for groups of classes that all stem from the same source.
@@ -24,17 +20,18 @@ public class LootConversionManager<L, T extends LootAware<L>> {
 
     private final @NotNull ImmuTables<L> owner;
     private final @NotNull String keyLocation;
-    private final @Nullable BiFunction<JsonElement, ImmuTables<L>, T> fallbackDeserializer;
 
     private final @NotNull Map<String, LootConverter<L, ? extends T>> keyRegistry;
     private final @NotNull Map<Class<? extends T>, LootConverter<L, ? extends T>> classRegistry;
 
+    private final @NotNull List<ComplexLootConverter<L, T>> complexConverters;
+
     private LootConversionManager(@NotNull Builder<L, T> builder) {
         this.owner = builder.owner;
         this.keyLocation = builder.keyLocation;
-        this.fallbackDeserializer = builder.fallbackDeserializer;
         this.keyRegistry = Map.copyOf(builder.keyRegistry);
         this.classRegistry = Map.copyOf(builder.classRegistry);
+        this.complexConverters = builder.complexConverters;
     }
 
     /**
@@ -52,13 +49,6 @@ public class LootConversionManager<L, T extends LootAware<L>> {
     }
 
     /**
-     * @return the deserializer that is used if, at some point, a JSON element that isn't a JSON object needs to be deserialized
-     */
-    public @Nullable BiFunction<JsonElement, ImmuTables<L>, T> fallbackDeserializer() {
-        return fallbackDeserializer;
-    }
-
-    /**
      * @return the converter that is associated with the provided key
      */
     public @Nullable LootConverter<L, ? extends T> request(@NotNull String key) {
@@ -66,21 +56,28 @@ public class LootConversionManager<L, T extends LootAware<L>> {
     }
 
     /**
-     * Serializes the provided object into a JsonObject.
-     * @param n the object to serialize
-     * @return a JsonObject representing the serialized state of the provided object
-     * @param <N> the type of the object that will be serialized
+     * Serializes the provided object into a JSON element.
+     * @param s the object to serialize
+     * @return a JSON element representing the serialized state of the provided object
+     * @param <S> the type of the object that will be serialized
      * @throws LootParsingException if something happens during serialization or if a valid loot converter couldn't be found
      */
-    public <N extends T> @NotNull JsonObject serialize(@NotNull N n) throws LootParsingException {
-        JsonObject object = new JsonObject();
-        @SuppressWarnings("unchecked") // We, at this point, know that it must be a LootConverter<L, N>.
-        LootConverter<L, N> converter = (LootConverter<L, N>) this.classRegistry.get(n.getClass());
-        if (converter == null) {
-            throw new LootParsingException("Could not find a LootConverter for class '" + n.getClass() + "'");
+    public <S extends T> @NotNull JsonElement serialize(@NotNull S s) throws LootParsingException {
+        if (!complexConverters.isEmpty()) {
+            for (ComplexLootConverter<L, T> complexConverter : complexConverters) {
+                if (complexConverter.canSerialize(s, this)) {
+                    return complexConverter.serialize(s, this);
+                }
+            }
         }
+        @SuppressWarnings("unchecked") // We, at this point, know that it must be a LootConverter<L, S>.
+        LootConverter<L, S> converter = (LootConverter<L, S>) this.classRegistry.get(s.getClass());
+        if (converter == null) {
+            throw new LootParsingException("Could not find a LootConverter for class '" + s.getClass() + "'");
+        }
+        JsonObject object = new JsonObject();
         object.addProperty(this.keyLocation, converter.key());
-        converter.serialize(n, object, this.owner);
+        converter.serialize(s, object, this.owner);
         return object;
     }
 
@@ -90,24 +87,20 @@ public class LootConversionManager<L, T extends LootAware<L>> {
      * @return the instance of something extending {@code T} that was deserialized
      */
     public @NotNull T deserialize(@Nullable JsonElement element) throws LootParsingException {
-        if (element != null && element.isJsonObject()){
-            JsonObject object = element.getAsJsonObject();
-            String type = JsonUtils.getAsString(object.get(keyLocation));
-            if (type != null){
-                LootConverter<L, ? extends T> t = this.keyRegistry.get(type);
-                if (t != null) {
-                    return t.deserialize(object, this.owner);
+        if (!complexConverters.isEmpty()) {
+            for (ComplexLootConverter<L, T> complexConverter : complexConverters) {
+                if (complexConverter.canDeserialize(element, this)) {
+                    return complexConverter.deserialize(element, this);
                 }
-                throw new LootParsingException("Could not find deserializer for type \"" + type + "\"!");
             }
         }
-        if (this.fallbackDeserializer != null) {
-            T t = this.fallbackDeserializer.apply(element, this.owner);
-            if (t != null) {
-                return t;
-            }
+        JsonObject object = JsonUtils.assureJsonObject(element, null);
+        String type = JsonUtils.assureString(object.get(keyLocation), keyLocation);
+        LootConverter<L, ? extends T> t = this.keyRegistry.get(type);
+        if (t == null) {
+            throw new LootParsingException("Could not find deserializer for type \"" + type + "\"!");
         }
-        throw new LootParsingException("Expected the provided element to not be null!");
+        return t.deserialize(object, this.owner);
     }
 
     /**
@@ -136,9 +129,9 @@ public class LootConversionManager<L, T extends LootAware<L>> {
 
         private ImmuTables<L> owner;
         private String keyLocation;
-        private BiFunction<JsonElement, ImmuTables<L>, T> fallbackDeserializer;
         private final @NotNull Map<String, LootConverter<L, ? extends T>> keyRegistry = new HashMap<>();
         private final @NotNull Map<Class<? extends T>, LootConverter<L, ? extends T>> classRegistry = new HashMap<>();
+        private final @NotNull List<ComplexLootConverter<L, T>> complexConverters = new ArrayList<>();
 
         private Builder() {}
 
@@ -155,12 +148,6 @@ public class LootConversionManager<L, T extends LootAware<L>> {
         }
 
         @Contract("_ -> this")
-        public @NotNull Builder<L, T> fallbackDeserializer(@NotNull BiFunction<JsonElement, ImmuTables<L>, T> fallbackDeserializer) {
-            this.fallbackDeserializer = fallbackDeserializer;
-            return this;
-        }
-
-        @Contract("_ -> this")
         public @NotNull Builder<L, T> addConverter(@NotNull LootConverter<L, ? extends T> converter) {
             if (this.keyRegistry.containsKey(converter.key())) {
                 throw new IllegalArgumentException("Cannot register value for key '" + converter.key() + "' as something with that key is already registered");
@@ -170,6 +157,12 @@ public class LootConversionManager<L, T extends LootAware<L>> {
             }
             this.keyRegistry.put(converter.key(), converter);
             this.classRegistry.put(converter.convertedClass(), converter);
+            return this;
+        }
+
+        @Contract("_ -> this")
+        public @NotNull Builder<L, T> addComplexConverter(@NotNull ComplexLootConverter<L, T> converter) {
+            this.complexConverters.add(converter);
             return this;
         }
 
