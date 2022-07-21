ae8ae75bb7019f851881f3719046bf3bb67d179c
GoldenStack <goldenfire64yt@gmail.com>
2022-07-21 17:39:30 -0500

    Add LootCondition, LootEntry, LootModifier, and LootNumber
    Signed-off-by: GoldenStack <goldenfire64yt@gmail.com>

---
+++ Core/src/main/java/dev/goldenstack/loot/structure/LootEntry.java
@@ -0,0 +1,47 @@
+package dev.goldenstack.loot.structure;
+
+import dev.goldenstack.loot.context.LootContext;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Range;
+
+import java.util.List;
+
+/**
+ * An entry in a loot table that can generate a list of {@link Option options} that each have their own loot and weight.
+ * @param <L> the loot item type
+ */
+public interface LootEntry<L> {
+
+    /**
+     * Generates any number of possible options to choose from when generating loot.
+     * @param context the context object, to use if required
+     * @return a list, with undetermined mutability, containing the options that were generated
+     */
+    @NotNull List<Option<L>> requestOptions(@NotNull LootContext context);
+
+    /**
+     * An option, generated from an entry, that could potentially be chosen.
+     * @param <L> the loot item type
+     */
+    interface Option<L> {
+
+        /**
+         * Calculates the weight of this option, to be used when choosing which options should be used.
+         * This number should not be below 1.<br>
+         * When using the result of this method, be aware of the fact that it's valid for implementations of this method
+         * to return different values even when the provided context is the identical.
+         * @param context the context object, to use if required
+         * @return the weight of this option
+         */
+        @Range(from = 1L, to = Long.MAX_VALUE) long getWeight(@NotNull LootContext context);
+
+        /**
+         * Generates a list of loot based on the provided context.
+         * @param context the context object, to use if required
+         * @return the list of loot that was generated
+         */
+        @NotNull List<L> generate(@NotNull LootContext context);
+
+    }
+
+}
---
+++ Core/src/main/java/dev/goldenstack/loot/structure/LootModifier.java
@@ -0,0 +1,21 @@
+package dev.goldenstack.loot.structure;
+
+import dev.goldenstack.loot.context.LootContext;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * A function that allows loot to pass through it, potentially making modifications.
+ * @param <L> the loot item type
+ */
+public interface LootModifier<L> {
+
+    /**
+     * Performs any mutations on the provided object and returns the result.
+     * @param input the input item to this modifier. When providing this parameter, be aware of the fact that the
+     *              parameter itself could potentially be modified.
+     * @param context the context object, to use if required
+     * @return the modified form of the input
+     */
+    @NotNull L modify(@NotNull L input, @NotNull LootContext context);
+
+}
