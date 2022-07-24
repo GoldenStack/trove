70f4ed57bc6634ff24e2bcd3c07e3087ecc321d9
GoldenStack <goldenfire64yt@gmail.com>
2022-07-24 01:49:49 -0500

    Add Utils and move LootEntry.generateLoot
    Signed-off-by: GoldenStack <goldenfire64yt@gmail.com>

--- Core/src/main/java/dev/goldenstack/loot/structure/LootEntry.java
+++ Core/src/main/java/dev/goldenstack/loot/structure/LootEntry.java
@@ -5,7 +5,6 @@ import dev.goldenstack.loot.generation.LootGenerator;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Range;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -37,58 +36,6 @@ public interface LootEntry<L> {
          */
         @Range(from = 1L, to = Long.MAX_VALUE) long getWeight(@NotNull LootGenerationContext context);
 
-    }
-
-    /**
-     * Generates loot from the provided entries. This process is repeated {@code rolls} times. For each of the provided
-     * entries, {@link #requestOptions(LootGenerationContext) LootEntry#requestOptions} is only called once and
-     * {@link LootEntry.Option#getWeight(LootGenerationContext) LootEntry.Option#getWeight} is only called once, so it
-     * is theoretically safe for them to return different results even if the context is the same.<br>
-     * To be specific, for each roll, the entries are consolidated into options via #requestOptions, a random option
-     * from them is determined via each option's weight, and that option is used to generate loot.<br>
-     * This is in the core library because, although it's not the only way to generate loot, it's a pretty
-     * straightforward way that will usually be the method used.
-     * @param entries the entries to generate for
-     * @param rolls the number of times to generate loot from the entries
-     * @param context the context object, to use if required
-     * @return the generated list of loot items
-     * @param <L> the loot item type
-     */
-    static <L> @NotNull List<L> generateLoot(@NotNull List<LootEntry<L>> entries, long rolls, @NotNull LootGenerationContext context) {
-        List<L> items = new ArrayList<>();
-        for (int i = 0; i < rolls; i++) {
-            // Weight and choices must be recalculated each time as their results theoretically may change
-            List<LootEntry.Option<L>> options = new ArrayList<>();
-            for (LootEntry<L> entry : entries) {
-                options.addAll(entry.requestOptions(context));
-            }
-
-            if (options.isEmpty()) {
-                continue;
-            }
-
-            long totalWeight = 0;
-            long[] lowerWeightMilestones = new long[options.size()];
-            for (int j = 0; j < options.size(); j++) {
-                lowerWeightMilestones[j] = totalWeight;
-                // Prevent the weight of this option from being less than 1
-                totalWeight += Math.max(1, options.get(j).getWeight(context));
-            }
-
-            long value = context.random().nextLong(0, totalWeight);
-
-            LootEntry.Option<L> option = options.get(options.size() - 1);
-
-            for (int j = 0; j < lowerWeightMilestones.length; j++) {
-                if (value >= lowerWeightMilestones[j]) {
-                    option = options.get(j);
-                    break;
-                }
-            }
-
-            items.addAll(option.generate(context));
-        }
-        return items;
     }
 
 }
