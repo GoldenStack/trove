50766777ad65e26ffc1c1420e6722419bfc5fe60
GoldenStack <goldenfire64yt@gmail.com>
2024-08-27 10:30:04 -0500

    Add LootTable and LootPool

--- src/main/java/net/goldenstack/loot/LootTable.java
+++ src/main/java/net/goldenstack/loot/LootTable.java
@@ -3,11 +3,25 @@ package net.goldenstack.loot;
 import net.minestom.server.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public record LootTable() implements LootGenerator {
+/**
+ * A loot table.
+ * @param pools the pools that generate items in this table
+ * @param functions the functions applied to each output item of this table
+ */
+public record LootTable(@NotNull List<LootPool> pools, @NotNull List<LootFunction> functions) implements LootGenerator {
     @Override
     public @NotNull List<ItemStack> apply(@NotNull LootContext context) {
-        return List.of();
+        List<ItemStack> items = new ArrayList<>();
+
+        for (var pool : pools) {
+            for (var item : pool.apply(context)) {
+                items.add(LootFunction.apply(functions, item, context));
+            }
+        }
+
+        return items;
     }
 }
